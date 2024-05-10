cc7b5d79723e85c48456160889ead1890f144347
Richard Jesus Guerrero Deniz <Richard_Mirra2@outlook.com>
2024-05-10 04:50:34 -0600

    Actualizacion

--- tests/unit/modules/daybook/components/EntryList.spec.js
+++ tests/unit/modules/daybook/components/EntryList.spec.js
@@ -4,16 +4,16 @@ import { journalState } from "../../../mock-data/test-journal-state"
 import { shallowMount } from "@vue/test-utils"
 import EntryList from "@/modules/daybook/components/EntryList.vue"
 
-const createVuexStore = (initialState) => {
-    return createStore({
+
+const createVuexStore = (initialState) =>
+    createStore({
         modules: {
             journal: {
-                ...journalModule,
-                state: { ...initialState }
+                ...journalModule, //Desestructuramos todo lo que journalModule contenga
+                state: { ...initialState } //Sobreescribimos el state con lo que haya en initialState
             }
         }
     })
-}
 
 describe('Pruebas en el EntryList', () => {
     const store = createVuexStore(journalState)
